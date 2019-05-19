4dc9f7c921a05a6c62bbb8fc4a056178fdcf9706
Denis Alayza <denis.alayza@gmail.com>
2019-05-19 13:47:23 -0300

    Structure Project with Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PagesComponent } from './pages/pages.component';
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
+import { NonpagefoundComponent } from './shared/nonpagefound/nonpagefound.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+
+const appRoutes: Routes = [
+  {
+      path: '',
+      component: PagesComponent,
+      children: [
+          { path: 'dashboard', component: DashboardComponent },
+          { path: 'progress', component: ProgressComponent },
+          { path: 'graficas1', component: Graficas1Component },
+          { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+      ]
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NonpagefoundComponent }
+];
+
+/* @NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { }*/
+
+export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } );
